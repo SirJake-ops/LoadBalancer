4df53f747be77f32a83223e1010d2c329304a174
Jake Pagan <jacob.pagan10@gmail.com>
2026-06-04 10:12:51 -0500

    Just added some test for coverage

--- src/balancing/mod.rs
+++ src/balancing/mod.rs
@@ -1,1 +1,15 @@
 mod round_robin;
+
+use crate::config::BackendConfig;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum BalanceError {
+    NoBackendsAvailable,
+}
+
+pub trait BalancingStrategy {
+    fn select_backend(
+        &mut self,
+        candidates: &[BackendConfig],
+    ) -> Result<BackendConfig, BalanceError>;
+}
